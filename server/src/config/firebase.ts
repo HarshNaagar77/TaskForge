cc594fb22eb8963817c7d3a9c19f189f61940e34
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2025-06-25 21:55:22 +0530

    Update

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_JSON!);
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    }),
   });
 }
 

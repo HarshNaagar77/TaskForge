17743158383b8159c33d296a3f262565eb167995
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2025-06-26 17:37:36 +0530

    update

--- client/components/ui/ProgressBar.tsx
+++ client/components/ui/ProgressBar.tsx
@@ -15,7 +15,7 @@ const Progress = React.forwardRef<
   >
     <div
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (((value || 0) / max) * 100)}%)` }}
     />
   </div>
 ));

9e2215e92d9b133fff4bda3bcbea89d20150e96d
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2025-06-26 23:07:55 +0530

    update

--- client/app/lib/axios.ts
+++ client/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL  || "https://taskforge-1-kl9n.onrender.com/api" ,
+  baseURL:"https://taskforge-1-kl9n.onrender.com/api" ,
 });
 
 export default api;
